0ccd86e927939dc0a871205cee45f669aae4c739
Nikolai <nikolai.shulga@scandiweb.com>
2022-08-26 09:34:49 +0300

    add text content const

---
+++ src/utils/textContent.ts
@@ -0,0 +1,33 @@
+export interface ItextContent {
+  title: string;
+  description: string;
+  course: string;
+  projectType: string;
+  industry: string;
+}
+export interface IserviceDescriptionContent {
+  [key: string]: ItextContent;
+  corp: ItextContent;
+  market: ItextContent;
+}
+
+const textContent: IserviceDescriptionContent = {
+  corp: {
+    title: 'Создание корпоративного сайта для холдинга «АМКОДОР»',
+    description: `Разработать и запустить корпоративный сайт для холдинга “АМКОДОР” 
+        для развития дилерской сети на рынках Беларуси и стран СНГ.`,
+    course: 'WEB-разработка',
+    projectType: 'Корпоративные сайты',
+    industry: 'Производство, Торговля',
+  },
+  market: {
+    title: 'Создание маркетплейса для бизнеса по перепродаже одежды',
+    description: `Brands&Charity — благотворительная онлайн — платформа для перепродажи брендовых вещей, 
+        цель которой превратить ненужные одним людям вещи в полезный ресурс для других.`,
+    course: 'WEB-разработка',
+    projectType: 'Интернет-магазины ',
+    industry: 'Торговля',
+  },
+};
+
+export default textContent;
