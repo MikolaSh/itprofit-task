e309e714e5f7c164281e20de90bfb4badb43d4f7
Nikolai <nikolai.shulga@scandiweb.com>
2022-08-26 09:23:47 +0300

    add customCursor movment

--- src/components/cursor/cursor.ts
+++ src/components/cursor/cursor.ts
@@ -1,7 +1,15 @@
 import './cursor.scss';
 
 const createCustomCursor = () => {
-    console.log('cursor');
+  const cursor: HTMLDivElement | null = document.querySelector('.cursor');
+
+  document.addEventListener('mousemove', (e: MouseEvent) => {
+    const posX: number = e.clientX;
+    const posY: number = e.clientY;
+
+    (<HTMLDivElement>cursor).style.left = `${posX}px`;
+    (<HTMLDivElement>cursor).style.top = `${posY}px`;
+  });
 };
 
 export default createCustomCursor;
