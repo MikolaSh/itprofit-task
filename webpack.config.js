96ae27cddda6d34f1d1ef003849f8320bdf9d01c
Nikolai <nikolai.shulga@scandiweb.com>
2022-08-26 08:29:12 +0300

    configured webpack plugins, assets configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[name][ext]',
   },
   module: {
     rules: [
@@ -13,8 +16,30 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'itprofit-task',
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+  ],
   resolve: {
     extensions: ['.ts', '.js'],
   },
